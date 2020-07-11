3e9c683557cca4d3995be1808af74007e3573afb
Wesley Robb <wesrobb86@gmail.com>
2020-07-11 21:18:16 +0200

    Use new vcpkg location on mac

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p build
-cmake -DCMAKE_TOOLCHAIN_FILE=/usr/local/Cellar/vcpkg/2020.04/libexec/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-osx -S . -B build 
+cmake -DCMAKE_TOOLCHAIN_FILE=~/src/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-osx -S . -B build 
